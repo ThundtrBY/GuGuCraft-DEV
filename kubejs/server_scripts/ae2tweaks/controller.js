6d7e5ff3696abc3cf0d4ef3a69baf53fe229d842
ThundtrBY <757306685@qq.com>
2024-05-17 16:58:44 +0800

    multiple recipe tweak&fix

--- kubejs/server_scripts/ae2tweaks/controller.js
+++ kubejs/server_scripts/ae2tweaks/controller.js
@@ -1,15 +1,17 @@
 ServerEvents.recipes(e => {
+    e.remove({output:'ae2:controller'})
     e.recipes.create.mechanical_crafting('ae2:controller', [
       'PMPMP',
-      'MFCFM',
+      'MFEFM',
       'PCICP',
-      'MFCFM',
+      'MFEFM',
       'PMPMP'
     ], {
       C: 'kubejs:calculation_mechanism',
+      E: 'ae2:engineering_processor',
       F: 'ae2:fluix_block',
       M: 'ae2:smooth_sky_stone_block',
       P: 'tfmg:plastic_sheet',
-      I: 'kubejs:integrational_mechanism'
+      I: 'kubejs:computation_matrix'
     })
   })
--- kubejs/server_scripts/ae2tweaks/things.js
+++ kubejs/server_scripts/ae2tweaks/things.js
@@ -1,22 +1,9 @@
 ServerEvents.recipes(e => {
     let transmatrix = 'kubejs:incomplete_matrix'
     e.remove({output:'ae2:matter_cannon'})
-    e.shaped(
-        Item.of('ae2:mysterious_cube', 1),
-        [ 
-          'EAE', 
-          'BFD',
-          'ECE'  
-        ],
-        {
-          A: 'ae2:calculation_processor_press',
-          B: 'ae2:engineering_processor_press',
-          C: 'ae2:silicon_press',
-          D: 'ae2:logic_processor_press',
-          E: 'ae2:smooth_sky_stone_block',
-          F: 'ae2:fluix_block'
-        }
-    )
+    e.remove({output:'ae2:sky_dust'})
+    e.remove({output:'ae2:silicon'})
+    e.recipes.create.crushing(['ae2:sky_stone_block', Item.of('ae2:sky_dust').withChance(0.5)], 'ae2:sky_stone_block').processingTime(200)
     e.recipes.create.sequenced_assembly([
 		Item.of('kubejs:incomplete_matrix')
 	], 'ae2:printed_silicon', [ 
