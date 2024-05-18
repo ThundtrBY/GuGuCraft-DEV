614bbd7d8c55f6bc01d33057d9970594ef623174
ThundtrBY <757306685@qq.com>
2024-05-18 15:35:53 +0800

    fix kuayue recipe

--- kubejs/server_scripts/createtweak/train_machine.js
+++ kubejs/server_scripts/createtweak/train_machine.js
@@ -1,44 +1,39 @@
+ServerEvents.tags('item', event =>{
+  event.add('kubejs:kuayue_blocks','kubejs:train_machine')
+  Ingredient.of('/kuayue.*/').itemIds.forEach(i =>{
+    event.add('kubejs:kuayue_blocks',i)
+  })
+})
 ServerEvents.recipes(event => {
 function cut(result) {
 event.stonecutting(result,'kubejs:train_machine')
 }
-event.remove({ id: 'create:crushing/obsidian' })
-Ingredient.of('/kuayue.*/').itemIds.forEach(i => {
-cut('64x '+i)
+event.remove({mod: 'kuayue'})
+cut('64x kuayue:standard_track')
+cut('64x kuayue:tieless_track')
+cut('64x kuayue:ballastless_track')
+event.custom({
+  "type": "chipped:mason_table",
+  "tags": ["kubejs:kuayue_blocks"]
 })
-cut('64x create:track')
-event.custom({
-    "type": "create:crushing",
-    "ingredients": [
-      {
-        "item": "minecraft:obsidian"
-      }
-    ],
-    "processingTime": 500,
-    "results": [
-      {
-        "chance": 0.25,
-        "item": "create:powdered_obsidian"
-      },
-      {
-        "item": "minecraft:obsidian"
-      }
-    ]
-  })
-event.custom({
-    "type": "create:item_application",
-    "ingredients": [
-      {
-        "item": "create:railway_casing"
-      },
-      {
-        "item": "kubejs:iron_saw"
-      }
-    ],
-    "results": [
-      {
-        "item": "kubejs:train_machine"
-      }
-    ]
-  }).damageIngredient('kubejs:iron_saw')
+//Ingredient.of('/kuayue.*/').itemIds.forEach(i => {
+//cut('64x '+i)
+//})
+event.recipes.create.item_application("kubejs:train_machine",["create:railway_casing","kubejs:iron_saw"]).damageIngredient("kubejs:iron_saw")
+//event.custom({
+//    "type": "create:item_application",
+//    "ingredients": [
+//      {
+//        "item": "create:railway_casing"
+//      },
+//      {
+//        "item": "kubejs:iron_saw"
+//      }
+//    ],
+//    "results": [
+//      {
+//        "item": "kubejs:train_machine"
+//      }
+//    ]
+//  }).damageIngredient('kubejs:iron_saw')
 })
